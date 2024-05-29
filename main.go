52f58ce5512b6a5e9b5a7fe5e03ed756434d537b
Mauricio Lay <mlay@sodimac.cl>
2024-05-29 14:30:35 -0400

    version 2

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/godesde0/variables"
 )
 
 func main() {
 	variables.MuestroEnteros()
+	variables.RestoVariables()
+	variables.Nombre = "otro nombre"
+	fmt.Println(variables.Nombre)
+
 }
